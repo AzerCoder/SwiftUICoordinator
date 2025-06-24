e869f13b9d45ba3b4201c5f17acf86c91433ed03
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2025-06-24 14:01:22 +0500

    Initial Commit

---
+++ SwiftUICoordinator/SwiftUICoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoordinatorApp.swift
+//  SwiftUICoordinator
+//
+//  Created by A'zamjon Abdumuxtorov on 24/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
